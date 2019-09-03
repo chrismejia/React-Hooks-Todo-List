02e84af59f0a44f7c1e06fd4b17062c2fc7456db
Christian Mejia <chrismejia10@gmail.com>
2019-09-02 23:10:11 -0400

    Remove inline tasks; Import todoList with new date field; Prep for new additions

--- src/App.js
+++ src/App.js
@@ -1,42 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Todo from "./component/Todo";
 import TodoForm from "./component/TodoForm";
+import todoList from "./component/todoList.json";
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      text: "Learn about React Hooks",
-      isCompleted: false
-    },
-    {
-      text: "Visit Rich",
-      isCompleted: false
-    },
-    {
-      text: "Pick something to eat for lunch",
-      isCompleted: false
+  const [todos, setTodos] = useState(todoList);
+
+  const [lastCompletedIdx, setLastCompletedIdx] = useState(0);
+
+  useEffect(() => {
+    for (let i = 0; i < todos.length; i++) {
+      let currTodo = todos[i];
+
+      if (!currTodo.isCompleted) {
+      }
     }
-  ]);
+  });
 
   const addTodo = text => {
     const newTodos = [...todos, { text }];
     setTodos(newTodos);
   };
 
+  // Spread list of todos in array
+  // Mark active todo as completed
+  // Capture active and remove it from newTodos array
+  // Redefine newTodos by spreading newTodos array
+  // and adding finishedTodo at end; keep immutable
+  // Update todos with updated newTodos array
   const completeTodo = index => {
-    // Spread list of todos in array
-
-    const newTodos = [...todos];
+    let newTodos = [...todos];
     newTodos[index].isCompleted = true;
-    const [newPosition] = newTodos.splice(index, 1);
-    newTodos.push(newPosition);
+    const [finishedTodo] = newTodos.splice(index, 1);
+    newTodos = [...newTodos, finishedTodo];
     setTodos(newTodos);
   };
 
   const uncompleteTodo = index => {
     const newTodos = [...todos];
     newTodos[index].isCompleted = false;
+    const uncheckedTodo = newTodos.splice(index, 1);
+
+    for (let i = 0; i < newTodos.length; i++) {
+      let currTodo = newTodos[i];
+      if (currTodo.isCompleted) {
+        //insert one before
+      }
+    }
+
     setTodos(newTodos);
   };
 
@@ -53,7 +65,7 @@ function App() {
   return (
     <div className="app">
       <div className="todo-list">
-        <div class="todo-add">
+        <div className="todo-add">
           <TodoForm addTodo={addTodo} />
         </div>
         {todos.map((todo, index) => (
